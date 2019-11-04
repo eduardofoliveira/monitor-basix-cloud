5d24791146aa9d9261351439e4b186ed194c3080
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-11-04 14:00:40 -0300

    Verificando erros

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
 
   conn.api(
     //"originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/8981@cloud.cloudcom.com.br)",
-    "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/IVR_Monitora18@monitora18.cloudcom.com.br 99998888 XML public",
+    "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/500001@monitora18.cloudcom.com.br 99998888 XML public",
     result => {
       let [status, call_id] = result.body.split(" ");
 
