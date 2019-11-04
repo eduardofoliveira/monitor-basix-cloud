f0789365a3321846fe30f4a83ea5abca74a372c8
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-11-04 12:28:20 -0300

    Discando

--- index.js
+++ index.js
@@ -3,31 +3,11 @@ const esl = require("modesl");
 conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   conn.events("json", "all");
 
-  // conn.execute(
-  //   "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/Luiz@cloud.cloudcom.com.br)",
-  //   function(res) {
-  //     console.log(res.getBody());
-  //   }
-  // );
-
-  // conn.execute(
-  //   "originate",
-  //   "{origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/Luiz@cloud.cloudcom.com.br)",
-  //   function(res) {
-  //     console.log(res.getBody());
-  //   }
-  // );
-
-  // conn.originate(
-  //   "{origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge{origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/Luiz@cloud.cloudcom.com.br",
-  //   function(res) {
-  //     console.log(res.getBody());
-  //   }
-  // );
   conn.api(
     "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/8951@cloud.cloudcom.com.br)",
     result => {
-      console.log(result);
+      let [status, call_id] = result.body.split(" ");
+      console.log(status, call_id);
     }
   );
 
