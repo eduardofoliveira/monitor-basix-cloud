a9b4e203e8d6f9b9dd2819fd7b202d97bb483940
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-11-04 12:18:56 -0300

    Discando

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   //   }
   // );
   conn.api(
-    "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge{origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/Luiz@cloud.cloudcom.com.br",
+    "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/8951@cloud.cloudcom.com.br)",
     result => {
       console.log(result);
     }
