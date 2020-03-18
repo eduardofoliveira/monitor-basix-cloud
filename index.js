0662227ab4bc1fef114d13b3c78f4f688a841baa
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-18 18:45:04 -0300

    Adição monitor Allianz

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
     conn.api(
       //"originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/basix-cloud/Eduardo@cloud.cloudcom.com.br &bridge({origination_caller_id_number=551112345678}sofia/gateway/basix-cloud/8981@cloud.cloudcom.com.br)",
       "originate {origination_caller_id_number=551112345678,bridge_generate_comfort_noise=true}sofia/gateway/astpp/3010500003@54.207.81.171 99998888 XML public",
-      result => {
+      async result => {
         let [status, call_id] = result.body.split(" ");
 
         if (status === "+OK") {
